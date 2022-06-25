597d2f6a7e40ffed46bedf75f1f5b36cd202406a
Liew Chern Jin <69305479+codyy6@users.noreply.github.com>
2022-06-25 10:49:32 +0800

    changed min height to full screen

--- src/components/pages/CV/Cv.js
+++ src/components/pages/CV/Cv.js
@@ -3,7 +3,9 @@ import "./Cv.css"
 const Cv = () => {
     document.title="Cody Liew's CV"
   return (
-    <iframe src="https://drive.google.com/file/d/1zj_s5KYHhbSQJzsx5j7u21BqzmEc28Ti/preview" allow="autoplay"/>
+    <div className='cv'>
+      <iframe src="https://drive.google.com/file/d/1zj_s5KYHhbSQJzsx5j7u21BqzmEc28Ti/preview" allow="autoplay"/>
+    </div>
   )
 }
 
