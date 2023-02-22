69518308bf9f94fcb75f7e4cff2369aac4f09bcf
Liew Chern Jin <69305479+codyy6@users.noreply.github.com>
2023-02-22 12:16:30 +0800

    added Bitcoin review

--- src/components/pages/Research/ResearchInfo.js
+++ src/components/pages/Research/ResearchInfo.js
@@ -1,7 +1,7 @@
 const details =[{
     img:"https://drive.google.com/uc?export=view&id=1FLoIKaORjPm4niyI3H_vgO30h9eUlRMZ",
-    title:"Basic Introduction to Bitcoin (under progress)",
+    title:"A Review On Bitcoin",
     alt:"Bitcoin Image",
-    link:""
+    link:"https://drive.google.com/file/d/1pm3J1t2tYETDi6G5LW0M6Ki03s0kgaIc/view?usp=share_link"
 }]
 export default details;
